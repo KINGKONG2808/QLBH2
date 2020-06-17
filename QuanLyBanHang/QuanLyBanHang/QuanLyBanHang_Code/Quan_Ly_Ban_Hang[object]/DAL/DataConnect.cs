80251522e161622524f2ff1991d65ed0689666c3
HungVV <vuhung2881999@gmail.com>
2020-06-17 18:16:13 +0700

    update form login and home page

--- QuanLyBanHang/QuanLyBanHang/QuanLyBanHang_Code/Quan_Ly_Ban_Hang[object]/DAL/DataConnect.cs
+++ QuanLyBanHang/QuanLyBanHang/QuanLyBanHang_Code/Quan_Ly_Ban_Hang[object]/DAL/DataConnect.cs
@@ -13,10 +13,10 @@ namespace QL_BanHang.DAL
         public SqlConnection getConnect()
         {
             //(TUNG) Data Source=ADMIN\SQLEXPRESS;Initial Catalog=QL_BanHang;Integrated Security=True
-            //(HUNG) Data Source=VUHUNGA49E;Initial Catalog=QL_BANHANG2;Integrated Security=True
+            //(HUNG) Data Source=VUHUNGA49E;Initial Catalog=QL_BanHang2;Integrated Security=True
             //(HIEU) Data Source=DESKTOP-MVH8GU2;Initial Catalog=QL_BanHang;Integrated Security=True
             //(YEN) Data Source=ADMIN;Initial Catalog=QL_BANHANG2;Integrated Security=True
-            return new SqlConnection(@"null");
+            return new SqlConnection(@"Data Source=VUHUNGA49E;Initial Catalog=QL_BanHang2;Integrated Security=True");
         }
 
         public DataTable GetTable(string sql)
