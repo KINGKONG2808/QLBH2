a4bc4da740345377952cb926c2215ac748bf1135
HungVV <vuhung2881999@gmail.com>
2020-06-17 17:47:27 +0700

    update change

--- QuanLyBanHang/QuanLyBanHang/QuanLyBanHang_Code/Quan_Ly_Ban_Hang[object]/DAL/DataConnect.cs
+++ QuanLyBanHang/QuanLyBanHang/QuanLyBanHang_Code/Quan_Ly_Ban_Hang[object]/DAL/DataConnect.cs
@@ -13,16 +13,10 @@ namespace QL_BanHang.DAL
         public SqlConnection getConnect()
         {
             //(TUNG) Data Source=ADMIN\SQLEXPRESS;Initial Catalog=QL_BanHang;Integrated Security=True
-<<<<<<< HEAD
-            //(HUNG) Data Source=VUHUNGA49E;Initial Catalog=QL_BanHang;Integrated Security=True
+            //(HUNG) Data Source=VUHUNGA49E;Initial Catalog=QL_BANHANG2;Integrated Security=True
             //(HIEU) Data Source=DESKTOP-MVH8GU2;Initial Catalog=QL_BanHang;Integrated Security=True
             //(YEN) Data Source=ADMIN;Initial Catalog=QL_BANHANG2;Integrated Security=True
-            return new SqlConnection(@"Data Source=ADMIN;Initial Catalog=QL_BANHANG2;Integrated Security=True");
-=======
-            //(HUNG) Data Source=VUHUNGA49E;Initial Catalog=QL_BANHANG2;Integrated Security=True
-            //(HIEU) Data Source=DESKTOP-MVH8GU2;Initial Catalog=QL_BANHANG;Integrated Security=True
-            return new SqlConnection(@"Data Source=VUHUNGA49E;Initial Catalog=QL_BANHANG2;Integrated Security=True");
->>>>>>> 93a047537a59c7399d2b55583496fa8edee35346
+            return new SqlConnection(@"null");
         }
 
         public DataTable GetTable(string sql)
